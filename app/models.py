bcd1cc1deb2f79ba6f4b337646e05fcbc841e1c6
kamransumar <kamransumar@gmail.com>
2018-09-08 13:58:25 +0300

    added relationships for users

--- app/models.py
+++ app/models.py
@@ -9,15 +9,18 @@ class User(db.Model):
     image = db.Column(db.String(255))
     email = db.Column(db.String(255))
     password = db.Column(db.String(255))
-    pitches = db.Column(db.String(255))
+    pitches = db.relationship('Pitch', backref='author', lazy='dynamic')
+    comments = db.relationship('Comment', backref='comment', lazy='dynamic')
 
     def __repr__(self):
-        return f'User {self.id}, {self.name}, {self.username}, {self.image}, {self.email}, {self.password}, {self.pitches}'
+        return f'User {self.id}, {self.name}, {self.username}, {self.image}, {self.email}, {self.password}, {self.pitches}, {self.comment}'
 
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.column(db.Interger, db.ForeignKey('users.id'))
     ratings = db.Column(db.Interger)
     like = db.Column(db.Interger)
     dislike = db.Column(db.Interger)
@@ -30,12 +33,11 @@ class Comment(db.Model):
 class Pitch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String(255))
-    title = db.Column(db.String(255))
-    author = db.Column(db.String(255))
+
+    title = db.Column(db.String(100))
+    user_id = db.Column(db.String(100), db.ForeignKey('user.id'))
     content = db.Column(db.String)
     comment = db.Column(dp.String)
-
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
 
     def __repr__(self):
@@ -45,7 +47,7 @@ class Pitch(db.Model):
 class Category(db.Model):
     __tablename__ = 'categories'
     id = db.column(db.Integer, primary_key=True)
-    pitches = db.relationship('Pitch', backref='category')
+    pitches = db.relationship('Pitch', backref='category', lazy='dynamic')
 
     def __repr__(self):
         return f'Category {self.id}, {self.pitch}'
