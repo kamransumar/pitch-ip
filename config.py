238d47b0d5e3e51e051c4424adeb5b3c90b0b977
kamransumar <kamransumar@gmail.com>
2018-09-14 10:56:21 +0300

    3d attemo to deploy

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/pitchesdb'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/pitchesdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -32,7 +32,7 @@ class ProdConfig(Config):
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/pitchesdb'
     SECRET_KEY = 'asdf78tuegfjhdgfasfu'
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     pass
 
@@ -44,8 +44,8 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SECRET_KEY = 'asdf78tuegfjhdgfasfu'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/pitchesdb'
+    # SECRET_KEY = 'asdf78tuegfjhdgfasfu'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/pitchesdb'
 
     DEBUG = True
 
