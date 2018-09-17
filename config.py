2cc906d607efcad0a0644d74af244a9c3099b37a
kamransumar <kamransumar@gmail.com>
2018-09-17 11:59:24 +0300

    deploymnt300

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     '''
     General configuration parent class
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/blogdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
@@ -30,8 +29,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/blogdb'
-    # SECRET_KEY = 'jelly'
+    SECRET_KEY = 'asdf78tuegfjhdgfasfu'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
     pass
@@ -45,7 +43,6 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     # SECRET_KEY = 'jelly'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/blogdb'
 
     DEBUG = True
 
