c82c00c2e37efb2e79e2bc4ad758b3fe3862038a
kamransumar <kamransumar@gmail.com>
2018-09-17 11:01:34 +0300

    deployment attempt 2

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/pitchesdb'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://macuser:1234@localhost/pitchesdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
